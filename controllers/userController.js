3ecbfc466181408669d0d3a6b4501d75e6c27703
rahulb15 <rahul.baghel1508@gmail.com>
2022-06-24 15:42:10 +0530

    backendcompletion

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 import userModel from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-
+import transporter from '../config/emailConfig.js'
 
 
 class UserController{
@@ -80,6 +80,63 @@ class UserController{
         }else{
             res.send({"status":"failed","message":"All fields are required"});
         }
+    }
+
+    static loggedUser = async(req,res)=>{
+        res.send({"user":req.user})
+    }
+
+    static sendUserPasswordResetEmail = async(req,res)=>{
+        const {email} = req.body
+        if(email){
+            const user = await userModel.findOne({email:email});
+            if(user){
+                const secret = user._id + process.env.JWT_SECRET_KEY
+                const token = jwt.sign({userID:user._id}, secret, {expiresIn: "15m"});
+                const link = `http://localhost:3010/api/user/reset/${user._id}/${token}`;
+                console.log(link);
+                
+                // // Send Email
+                      let info = await transporter.sendMail({
+                       from: process.env.EMAIL_FROM,
+                       to: user.email,
+                       subject: "TestProject - Password Reset Link",
+                       html: `<a href=${link}>Click Here</a> to Reset Your Password`
+                   });
+
+
+                res.send({"status": "success", "message": "Password Reset Email Sent... Please Check Your Email","info": info});
+            }else{
+                res.send({"status":"failed","message":"Email Doesn't Exists"});
+
+            }         
+        } else{
+            res.send({"status":"failed","message": " Email fields are required"});
+        }
+    }
+    static userPasswordReset = async(req,res)=>{
+        const {password,password_confirmation} = req.body;
+        const {id,token} = req.params
+        const user = await userModel.findById(id);
+        const new_secret = user._id + process.env.JWT_SECRET_KEY;
+        try {
+            jwt.verify(token,new_secret);
+            if(password&&password_confirmation){
+                if (password!==password_confirmation) {
+                    res.send({"status":"failed","message": " New Password And Confirm New Password Doesn't Match"});
+                } else {
+                    const salt = await bcrypt.genSalt(10);
+                    const newHashPassword = await bcrypt.hash(password,salt);
+                    await userModel.findByIdAndUpdate(user._id, {$set:{password: newHashPassword}});
+                    res.send({"status": "success", "message": "Password Reset Successfully"});
+                }
+
+            }else{
+                res.send({"status":"failed","message": " All fields are required"});
+            }
+        } catch (error) {
+            console.log(error);
+            res.send({ "status": "failed", "message": "Invalid Token" });        }
     }
 }
 
