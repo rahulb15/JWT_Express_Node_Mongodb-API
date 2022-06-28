25754bf962f91d6dafae0d5b478629e9d1de67aa
rahulb15 <rahul.baghel1508@gmail.com>
2022-06-28 15:32:35 +0530

    format

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -4,54 +4,54 @@ import userModel from "../models/User.js";
 let token;
 let getId;
 
-var checkUserAuth = async(req,res,next)=>{
-    //let token 
-    const { authorization } = req.headers
-    if(authorization && authorization.startsWith('Bearer')){
-        try {
-            //Get Token from Header
-    
-            token = authorization.split(' ')[1];
-            //console.log(token);
-            const {userID} = jwt.verify(token,process.env.JWT_SECRET_KEY);
-            getId = userID;
-
-            //GET User from Token
-            const a = await userModel.findById(userID).select("token");
-            console.log(a.token);
-    
-            if(token === a.token){
-            req.user = await userModel.findById(userID).select("-password");
-            next();
-             }else{
-                res.send("You are logged out please login again");
-             }
-
-        } catch (error) {
-            console.log(error);
-            res.status(401).send({"status": "failed", "message": "Unauthorized User"});
-            //await userModel.findByIdAndUpdate(user._id, {$set:{status: "Active"}});
-
-        }
-    }
-    if(!token){
-        res.status(401).send({"status": "failed","messsage": "Unauthorized User, No Token"});
-    }
-}
-
-
-var deleteAuth = async(req,res,next)=>{
+var checkUserAuth = async (req, res, next) => {
+  //let token
+  const { authorization } = req.headers;
+  if (authorization && authorization.startsWith("Bearer")) {
     try {
+      //Get Token from Header
 
-        //req.user = await userModel.findById(getId).select("-password");
-        req.user = await userModel.findByIdAndUpdate(getId,{$set:{token:" ",status:"Inactive"}});
-        //console(req.user);
+      token = authorization.split(" ")[1];
+      //console.log(token);
+      const { userID } = jwt.verify(token, process.env.JWT_SECRET_KEY);
+      getId = userID;
+
+      //GET User from Token
+      const a = await userModel.findById(userID).select("token");
+      console.log(a.token);
+
+      if (token === a.token) {
+        req.user = await userModel.findById(userID).select("-password");
         next();
+      } else {
+        res.send("You are logged out please login again");
+      }
     } catch (error) {
-        res.status(401).send({"status": "failed","messsage": "Unauthorized User, No Token"});
+      console.log(error);
+      res.status(401).send({ status: "failed", message: "Unauthorized User" });
+      //await userModel.findByIdAndUpdate(user._id, {$set:{status: "Active"}});
     }
-}
+  }
+  if (!token) {
+    res
+      .status(401)
+      .send({ status: "failed", messsage: "Unauthorized User, No Token" });
+  }
+};
 
+var deleteAuth = async (req, res, next) => {
+  try {
+    //req.user = await userModel.findById(getId).select("-password");
+    req.user = await userModel.findByIdAndUpdate(getId, {
+      $set: { token: " ", status: "Inactive" },
+    });
+    //console(req.user);
+    next();
+  } catch (error) {
+    res
+      .status(401)
+      .send({ status: "failed", messsage: "Unauthorized User, No Token" });
+  }
+};
 
-
-export {checkUserAuth,deleteAuth}
+export { checkUserAuth, deleteAuth };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,31 +1,30 @@
 import express from "express";
 const router = express.Router();
 import UserController from "../controllers/userController.js";
-import {checkUserAuth,deleteAuth} from "../middlewares/auth-middleware.js";
+import { checkUserAuth, deleteAuth } from "../middlewares/auth-middleware.js";
 //import deleteAuth from "../middlewares/auth-middleware.js";
 
 //Route Level Middleware - To Protect Route
-router.use("/changepassword",checkUserAuth);
-router.use("/loggeduser",checkUserAuth);
-router.use("/deleteuser",checkUserAuth);
-router.use("/updateuser",checkUserAuth);
-router.use("/logout",checkUserAuth,deleteAuth);
-
-
+router.use("/changepassword", checkUserAuth);
+router.use("/loggeduser", checkUserAuth);
+router.use("/deleteuser", checkUserAuth);
+router.use("/updateuser", checkUserAuth);
+router.use("/logout", checkUserAuth, deleteAuth);
 
 //Public Routes
-router.post("/register",UserController.userRegistration);
-router.post("/login",UserController.userLogin);
-router.post("/send-reset-password-email",UserController.sendUserPasswordResetEmail);
-router.post("/reset-password/:id/:token",UserController.userPasswordReset);
-
+router.post("/register", UserController.userRegistration);
+router.post("/login", UserController.userLogin);
+router.post(
+  "/send-reset-password-email",
+  UserController.sendUserPasswordResetEmail
+);
+router.post("/reset-password/:id/:token", UserController.userPasswordReset);
 
 //Private Routes
-router.post("/changepassword",UserController.changeUserPassword);
-router.get("/loggeduser",UserController.loggedUser);
-router.delete("/deleteuser",UserController.userDelete);
-router.post("/updateuser",UserController.userUpdate);
-router.get("/logout",UserController.userLogout);
-
+router.post("/changepassword", UserController.changeUserPassword);
+router.get("/loggeduser", UserController.loggedUser);
+router.delete("/deleteuser", UserController.userDelete);
+router.post("/updateuser", UserController.userUpdate);
+router.get("/logout", UserController.userLogout);
 
 export default router;
